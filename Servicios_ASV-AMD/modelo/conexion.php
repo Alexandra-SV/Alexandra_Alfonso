0f146f5408535f78fcca020350711dd82264501e
AlfonsoMárquezDíaz <alfonsomarquezdiaz1@gmail.com>
2023-12-14 19:09:05 +0100

    add consultas

--- Servicios_ASV-AMD/modelo/conexion.php
+++
@@ -1,9 +0,0 @@
-<?php
-    include('../libs/bConfiguracion.php');
-    // Conectamos
-    $pdo = new PDO('mysql:host=' . $db_hostname . ';dbname=' . $db_nombre . '', $db_usuario, $db_clave);
-    // Realiza el enlace con la BD en utf-8
-    $pdo->exec("set names utf8");
-    //Accionamos el uso de excepciones
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-?>
