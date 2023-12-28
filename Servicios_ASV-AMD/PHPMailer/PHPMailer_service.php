90fec595332944a12a36494239e373b4d7a19016
AlfonsoMarquezDiaz <132476379+AlfonsoMarquezDiaz@users.noreply.github.com>
2023-12-28 19:16:37 +0100

    Update PHPMailer_service.php

--- Servicios_ASV-AMD/PHPMailer/PHPMailer_service.php
+++ Servicios_ASV-AMD/PHPMailer/PHPMailer_service.php
@@ -14,7 +14,7 @@
         $mail->Host = 'smtp.gmail.com';
         $mail->SMTPAuth = true;
         $mail->Username = 'asv.amd.php@gmail.com';
-        $mail->Password = '';
+        $mail->Password = ' ';
         $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
         $mail->Port = "465";
         //Configuración destinatarios
